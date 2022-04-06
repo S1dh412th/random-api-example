2f82383d166380c381c699d64cf5a68238e98622
S1dh412th <sidharth.kanwaria759@gmail.com>
2022-04-06 21:51:03 +0530

    Generated classes has been formatted

--- bored-api/src/main/java/com/example/bored_api/model/BoredResponse.kt
+++ bored-api/src/main/java/com/example/bored_api/model/BoredResponse.kt
@@ -3,14 +3,14 @@ package com.example.bored_api.model
 import com.google.gson.annotations.SerializedName
 
 
-data class BoredResponse (
+data class BoredResponse(
 
-  @SerializedName("activity"      ) var activity      : String? = null,
-  @SerializedName("accessibility" ) var accessibility : Double? = null,
-  @SerializedName("type"          ) var type          : String? = null,
-  @SerializedName("participants"  ) var participants  : Int?    = null,
-  @SerializedName("price"         ) var price         : Double? = null,
-  @SerializedName("link"          ) var link          : String? = null,
-  @SerializedName("key"           ) var key           : String? = null
+    @SerializedName("activity") var activity: String? = null,
+    @SerializedName("accessibility") var accessibility: Double? = null,
+    @SerializedName("type") var type: String? = null,
+    @SerializedName("participants") var participants: Int? = null,
+    @SerializedName("price") var price: Double? = null,
+    @SerializedName("link") var link: String? = null,
+    @SerializedName("key") var key: String? = null
 
 )
